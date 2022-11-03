f453175ec40a0f263ff09bad4f7a967943e7af9a
emadco88 <91788470+emadco88@users.noreply.github.com>
2022-11-03 11:33:30 +0200

    Add files via upload

---
+++ upper_baltoe/__manifest__.py
@@ -0,0 +1,5 @@
+{
+    'name': 'Uppermedic Baltoe',
+    'depends': ['base'],
+    'data': ['views/views.xml', 'views/menus.xml', ]
+}
---
+++ upper_baltoe/models/models.py
@@ -0,0 +1,148 @@
+from odoo import api, fields, models
+
+
+class Customers(models.Model):
+    _name = 'ub_customer'
+
+    name = fields.Char()
+    commercial_reg = fields.Char()
+    tax_no = fields.Char()
+    nid = fields.Char()
+    address = fields.Char()
+    description = fields.Text()
+    first_deal_date = fields.Date()
+    company = fields.Selection(selection=[('upper', 'Upper Medic'), ('baltoe', 'Baltoe'), ('common', 'Common'), ], )
+
+    eval = fields.Integer()
+
+    att_files = fields.Binary(string="Attachments", )
+
+
+class Services(models.Model):
+    _name = 'ub_service'
+    _description = 'Services'
+
+    name = fields.Char()
+    service_type = fields.Selection(selection=[('one', 'One Time'), ('subs', 'Subscription'), ])
+    cost = fields.Float()
+    price = fields.Float()
+
+
+class Contracts(models.Model):
+    _name = 'ub_contract'
+    _description = 'Contracts'
+
+    name = fields.Char()
+    company = fields.Selection(selection=[('upper', 'Upper Medic'), ('baltoe', 'Baltoe'), ])
+    customer_id = fields.Many2one('ub_customer')
+    contract_type = fields.Selection(selection=[('one', 'One Time'), ('subs', 'Subscription'), ])
+    service_id = fields.Many2one('ub_service')
+    estimated_cost = fields.Float()
+    total_price = fields.Float()
+    downpayment = fields.Float()
+    installment = fields.Float()
+    periodicity = fields.Integer()
+    start = fields.Date()
+    end = fields.Date()
+    creator_id = fields.Many2one('ub_employee')
+    att_files = fields.Binary()
+    status = fields.Selection(selection=[('active', 'Active'), ('inactivated', 'Inactivated'),
+                                         ('terminated', 'Terminated'), ('expired', 'Expired')])
+
+
+class Department(models.Model):
+    _name = 'ub_department'
+    _description = 'Department'
+
+    name = fields.Char()
+
+
+class Job(models.Model):
+    _name = 'ub_job'
+    _description = 'Jobs'
+    name = fields.Char()
+
+
+class Employees(models.Model):
+    _name = 'ub_employee'
+    _description = 'Employees'
+
+    name = fields.Char()
+    user_id = fields.Many2one('res.users')
+    birthdate = fields.Date()
+    qualification = fields.Char()
+    job_id = fields.Many2one('ub_job')
+    department_id = fields.Many2one('ub_department')
+    company = fields.Selection(selection=[('upper', 'Upper Medic'), ('baltoe', 'Baltoe'), ('common', 'Common'), ], )
+    working_type = fields.Selection(selection=[('full', 'Full Time'), ('part', 'Part Time'), ('free', 'Freelance'), ], )
+    hour_price = fields.Float()
+    hiring_date = fields.Date()
+
+
+class Doctype(models.Model):
+    _name = 'ub_doctype'
+    _description = 'Document Types'
+    name = fields.Char()
+
+
+class AccGuide(models.Model):
+    _name = 'ub_accguide'
+    _description = 'AccGuide'
+
+    name = fields.Char()
+    is_company_account = fields.Boolean()
+    has_costcenter = fields.Boolean()
+    required_costcenter = fields.Boolean()
+    parent_id = fields.Many2one('ub_accguide')
+
+
+class Income(models.Model):
+    _name = 'ub_income'
+    _description = 'Income'
+
+    creation_date = fields.Date()
+    due_date = fields.Date()
+    contract_id = fields.Many2one('ub_contract')
+    customer_id = fields.Many2one(related='contract_id.customer_id')
+    service_id = fields.Many2one('ub_service')
+    creator_id = fields.Many2one('ub_employee')
+    settlement_status = fields.Boolean()
+    settlement_date = fields.Date()
+    collected_value = fields.Float()
+    bill_value = fields.Float()
+    attach = fields.Binary()
+    company_account_id = fields.Many2one('ub_accguide')
+    doctype = fields.Many2one('ub_doctype')
+    company = fields.Selection(selection=[('upper', 'Upper Medic'), ('baltoe', 'Baltoe')], )
+    active = fields.Boolean(default=True)
+
+
+class CostCenter(models.Model):
+    _name = 'ub_costcenter'
+    _description = 'CostCenter'
+
+    name = fields.Char()
+    nick_name = fields.Char()
+    id_no = fields.Char()
+
+
+class JE(models.Model):
+    _name = 'ub_je'
+    _description = 'JE'
+    _rec_name = 'explain'
+    explain = fields.Char('explain')
+    out_value = fields.Float()
+    in_value = fields.Float()
+    costcenter_id = fields.Many2one('ub_costcenter')
+    creation_date = fields.Date()
+    due_date = fields.Date()
+    contract_id = fields.Many2one('ub_contract')
+    customer_id = fields.Many2one(related='contract_id.customer_id')
+    service_id = fields.Many2one('ub_service')
+    creator_id = fields.Many2one('ub_employee')
+    company = fields.Selection(selection=[('upper', 'Upper Medic'), ('baltoe', 'Baltoe'), ('common', 'Common'), ], )
+    doctype = fields.Many2one('ub_doctype')
+    company_account_id = fields.Many2one('ub_accguide')
+    on_account = fields.Many2one('ub_accguide')
+    att_files = fields.Binary(string="Attachments", )
+    active = fields.Boolean(default=True)
