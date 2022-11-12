149b078b35c148b6e4247a21aad3b0caf72cbdec
OssamaAbdeen <ossamaabdeen0@gmail.com>
2022-11-12 12:31:11 +0200

    UpperMedic Baltoe Seperation

---
+++ baltoe/__manifest__.py
@@ -0,0 +1,6 @@
+{
+    'name': 'Baltoe',
+    'depends': ['base'],
+    'application': True,
+    'data': ['views/views.xml', 'views/menus.xml', 'security/ir.model.access.csv']
+}
---
+++ upper_medic/__manifest__.py
@@ -0,0 +1,6 @@
+{
+    'name': 'UpperMedic',
+    'depends': ['base'],
+    'application': True,
+    'data': ['views/views.xml', 'views/menus.xml', 'security/ir.model.access.csv']
+}
